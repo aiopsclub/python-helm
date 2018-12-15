7a5c15795d629110e25ca44fee9d8029dc2245a1
guoweikuang <673411814@qq.com>
2018-12-15 11:39:30 +0800

    remove some

--- pyhelm/utils/exceptions.py
+++ pyhelm/utils/exceptions.py
@@ -8,9 +8,3 @@ class CustomError(Exception):
 
     def __str__(self):
         return self.errorinfo
-
-
-class TemplateError(Exception):
-    def __init__(self, message):
-        super(TemplateError, self).__init__(message)
-        self.message = message
